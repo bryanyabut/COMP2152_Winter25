e81973e21afc8bc28f31e5af9f593a924862e4ef
Bryan Yabut <bryan.yabut@outlook.com>
2025-01-13 14:31:36 -0500

    fixed directory for labs

---
+++ labs/week1/lab1.py
@@ -0,0 +1,24 @@
+#Sample Coding Questions 01 Week 01
+#Bryan Yabut
+
+#Q2
+numbers = [1,4,7,9]
+
+#Q3
+a = 1
+b = 2
+c = 3
+d = 4
+e = (a - ((b ** c) // d)) + (a % c)
+
+#Q4
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+#Q5
+userAge = int(input("What is your age? "))
+userAge += 22
+print("Now showing the shop items filtered by age: ", userAge)
+
+
+
