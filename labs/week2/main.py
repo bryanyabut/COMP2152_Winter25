a19a609ca42db21c9ba3f5c1b76680be5e184cbc
Bryan Yabut <bryan.yabut@outlook.com>
2025-01-19 02:37:22 -0500

    added two lab week 2 files

--- labs/week2/main.py
+++ labs/week2/main.py
@@ -1,20 +1,38 @@
 import random
 
-def number_guessing_game():
+
+def number_gussing_game():
     targetNumber = random.randint(1, 100)
 
-    play = input("Do you want to play a number guessing game? (y/n) ").strip().lower()
+    play = input("Do you want to play number guessing game? (yes/no)").strip().lower()
 
-    attempts = 0
-    max_attempts = 10
+    attemps = 0
+    max_attemps = 10
 
-    while attempts < max_attempts:
+    while attemps < max_attemps:
         try:
-            return True
-        except:
-            print("")
-    play = input()
-    return True
+            userGuess = int(input("Enter your guess: "))
+            # attempts = attempts + 1
+            attemps += 1
+            if userGuess < targetNumber:
+                if abs(userGuess - targetNumber) <= 5:
+                    print("Too low! You're very close! Try again.")
+                else:
+                    print("Too low, try again.")
+            elif userGuess > targetNumber:
+                if abs(userGuess - targetNumber) <= 5:
+                    print("Too High! You're very close! Try again.")
+                else:
+                    print("Too high! Try again.")
+            else:
+                print(f"Congratulation! You've guessed it in {attemps} attempts.")
+                return
+        except ValueError:
+            print("Invalid input! Please enter a number between 1 and 100")
+    print(f"Game over! The target number was {targetNumber}.")
+
+
+number_gussing_game()
 
 # targetNumber = random.randint(1,100)
 # userGuess = 0
